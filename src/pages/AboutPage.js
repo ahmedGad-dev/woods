b0eb747dd3a758eccd4564d573784ed99e526d09
Ahmed Gad <ahmedamrgad12345@gmail.com>
2023-11-27 19:35:15 +0200

    about page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,24 +1,36 @@
 import React from 'react'
 import styled from 'styled-components'
-import Logo from '../components/Logo'
-import PageHero from '../components/PageHero'
+import  PageHero  from '../components/PageHero'
+import aboutImg from '../assets/img/branches-5.jpg'
+import {BiLogoInstagram} from 'react-icons/bi'
+import {GrFacebook} from 'react-icons/gr'
+import {BsTwitter} from 'react-icons/bs'
+import {FaTiktok} from 'react-icons/fa'
+
 const AboutPage = () => {
   return(
      <main>
-      <Wrapper className='page section section-center'>
-         <PageHero title='about' />
-         <Logo/>
+      <PageHero title='about'/>
+       <Wrapper className='page section section-center'> 
+         <img  src={aboutImg}  alt='AhmedGad-Dev' />
          <article>
            <div className='title'>
-             <h2> Story </h2>
-             <div className="underline"></div>           
+             <h2> Woods </h2>          
            </div>  
            <p>        
              Lorem ipsum dolor sit amet consectetur adipisicing elit.
              Laudantium dicta quasi dolor iusto. Iusto voluptas corporis excepturi beatae. Aspernatur, excepturi!
              Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
              Commodi minus tempore aliquam quod libero hic possimus iure distinctio qui maiores? 
-           </p>     
+             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore earum ipsa sit sequi placeat rerum expedita aspernatur harum nisi facilis.
+             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex dolorum rem incidunt hic quae quis laboriosam, libero quo alias. Tempora reprehenderit dolores ratione nesciunt non nisi quod odio libero vel!
+           </p>   
+           <div style={{display:'flex', flexDirection:'row', justifyContent:'space-between', width:'50%', marginTop:'15px'}}>   
+             <GrFacebook/>
+             <BiLogoInstagram/>
+             <BsTwitter/>
+             <FaTiktok/>
+           </div>
          </article>
       </Wrapper>
      </main>    
